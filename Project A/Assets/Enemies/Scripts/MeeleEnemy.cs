b2d1edde578c1e5ec1c621ef7bb16bb60aafedb5
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-08-12 07:46:52 +0200

    EnemySetup v0.2

--- Project A/Assets/Enemies/Scripts/MeeleEnemy.cs
+++ Project A/Assets/Enemies/Scripts/MeeleEnemy.cs
@@ -1,18 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MeeleEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector] public event EventHandler OnEnemyDamaged;
+    public float Health;
+    public float MaxHealth;
+
+    [Header("Components")]
+    private Animator anim;
+    private Image HealthBar;
+    private SpriteRenderer sp;
+    [Header("States")]
+    private bool IsDead = false;
+
+
+    private void Start()
     {
-        
+        Health = MaxHealth;
+        anim = GetComponent<Animator>();
+        HealthBar = GetComponentInChildren<Image>();
+        sp = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        anim.SetBool("IsDead", IsDead);
+        if (IsDead)
+            EnemyDissolve();
+    }
+   
+    private void EnemyDissolve()
+    {
+        Vector4 RGBA = sp.color;
+        RGBA -= new Vector4(0, 0, 0, Time.deltaTime);
+        transform.GetComponent<SpriteRenderer>().color = RGBA;
+        if (RGBA.w <= 0)
+            Destroy(gameObject);
+    }
+    public void EnemyReceiveDamage(float Damage)
+    {
+        if (IsDead)
+            return;
+       
+
+
+        Health -= Damage;
+        HealthBar.fillAmount = Health / MaxHealth;
+
+        if (Health <= 0)
+        {
+            //Dead
+            IsDead = true;
+            anim.SetTrigger("Death");
+            return;
+        }
+
+        anim.SetTrigger("Hit");
+
+        OnEnemyDamaged?.Invoke(this, EventArgs.Empty);
+
     }
 }
