2a8cac5138ce2961414d0372f5065c94b25294ff
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-09-01 16:12:25 +0200

    .

---
+++ Project A/Assets/Ball.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float BallSpeed=10f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    
+    private void FixedUpdate()
+    {
+        rb.velocity += new Vector2(BallSpeed*Time.deltaTime, rb.velocity.y);
+
+    }
+}
