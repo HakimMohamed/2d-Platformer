d1fd7d4c5f47830daa63e749d8590f8bd82fca4f
Abdelhakim mohamed <kemstroarab@gmail.com>
2022-09-01 20:07:36 +0200

    .

---
+++ Project A/Assets/Enviroment/cavefirstscene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class cavefirstscene : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
